6ba963a8ae5933f14df837f6fced10aee51ee0ed
jptcnde <jptcnde@gmail.com>
2019-12-29 12:35:23 +0800

    update test

--- src/components/ProgressModal/ProgressModal.test.js
+++ src/components/ProgressModal/ProgressModal.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitForElementToBeRemoved } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import ProgressModal from './ProgressModal';
 
 describe('Progress Modal Component', () => {
@@ -12,5 +13,22 @@ describe('Progress Modal Component', () => {
     const { getByTestId } = render(<ProgressModal open />);
 
     expect(getByTestId('progress-indicator')).toBeInTheDocument();
+  });
+
+  test('it should have disappear after some progress', async () => {
+    const { getByTestId, rerender } = render(<ProgressModal open />);
+
+    window.setTimeout(() => {
+      rerender(<ProgressModal open={false} />);
+    }, 500);
+
+    const expected = true;
+
+    // is hidden
+    const actual = await waitForElementToBeRemoved(() =>
+      expect(getByTestId('progress-modal'))
+    );
+
+    expect(actual).toBe(expected);
   });
 });
